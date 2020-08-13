2e44c217f63c8684a25a1770e8ebcb63f6c28ede
ahapsah30 <68361965+ahapsah30@users.noreply.github.com>
2020-08-13 18:59:24 +0800

    Update wabot.py

--- wabot.py
+++ wabot.py
@@ -6,7 +6,7 @@ class WABot():
     def __init__(self, json):
         self.json = json
         self.dict_messages = json['messages']
-       self.APIUrl = 'https://eu104.chat-api.com/instance161053/'
+        self.APIUrl = 'https://eu104.chat-api.com/instance161053/'
         self.token = '3wzj1xyi9yhiiye4'
         print(self.dict_messages)
    
@@ -61,7 +61,26 @@ class WABot():
                 answer = self.send_requests('sendMessage', data)
                 return answer
 
-    
+    def yts(self, chatID):
+        for message in self.dict_messages:
+            text = message['body']
+            import requests as r
+            import json
+            par = text[3:]
+            req= r.get('http://api.farzain.com/yt_search.php?id='+par+'&apikey=JsaChFteVJakyjBa0M5syf64z&')
+            js1 = req.json()[1]['title']
+            js2 = req.json()[1]['url']
+            js3 = req.json()[1]['videoThumbs']
+            js4 = req.json()[1]['videoId']
+            data = {
+                  "body": js3,
+                  "caption" : '🔎 *Hasil Pencarian Youtube Acak*\n\n*Judul Video* : '+js1+'\n\n*Url Video* : '+js2+'\n\n*Video ID* : '+js4,
+                  "filename": 'jpg',
+                  "chatId": chatID
+                  }
+
+            answer = self.send_requests('sendFile', data)
+            return answer 
 
     def start(self, chatID):
         data = {
@@ -97,7 +116,27 @@ class WABot():
             answer = self.send_requests('sendPTT', data)
             return answer
     
-    
+    def ig(self, chatID):
+        for message in self.dict_messages:
+            text = message['body']
+            tex = message['senderName']
+            import requests as r
+            import json
+            par = text[3:]
+            req= r.get('https://www.instagram.com/'+par+'/?__a=1')
+            js1 = req.json()["graphql"]["user"]["biography"]
+            js2 = req.json()["graphql"]["user"]["full_name"]
+            js3 = req.json()["graphql"]["user"]["edge_followed_by"]["count"]
+            js4 = req.json()["graphql"]["user"]["edge_follow"]["count"]
+            js5 = req.json()["graphql"]["user"]["profile_pic_url_hd"]
+            data = {
+                  "body": js5,
+                  "caption" : '🔎 *Hasil Pencarian Instagram* \n\n*Username* : '+par+'\n*Nama* : '+str(js2)+'\n*Bio* : '+str(js1)+'\n*Followers* : '+str(js3)+'\n*Following* :'+str(js4),
+                  "filename": 'png',
+                  "chatId": chatID
+                  }
+            answer = self.send_requests('sendFile', data)
+            return answer  
 
     def processing(self):
         if self.dict_messages != []:
@@ -127,8 +166,4 @@ class WABot():
                         return self.er(id)
                 else: return 'NoCommand'
 
-            
-
-
-
- 
+           
