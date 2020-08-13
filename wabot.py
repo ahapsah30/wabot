c588ee066877f2d5d7f6ad1c838636aad7418ffd
ahapsah30 <68361965+ahapsah30@users.noreply.github.com>
2020-08-13 20:25:40 +0800

    Update wabot.py

--- wabot.py
+++ wabot.py
@@ -81,8 +81,8 @@ class WABot():
         for message in self.dict_messages:
             text = message['body'] 
             data = {
-                  "body": 'https://checker.in/go/94281',
-                  "caption" : '🔎 *Hasil Pencarian Youtube Acak*\n\n*Judul Video* : ',
+                  "body": 'https://firebasestorage.googleapis.com/v0/b/app-chat-api-com.appspot.com/o/2020%2F8%2F13%2F1d71ccee-7e5d-4f0d-bdd2-7b4bccbf186b.mp4?alt=media&token=58974e5c-7af4-41fc-93ae-da820c3482c8',
+                  "caption" : 'ECKO SHOW - Mantan Sombong',
                   "filename": 'mp4',
                   "chatId": chatID
                   }
@@ -111,8 +111,8 @@ class WABot():
                     elif text[0].lower() == 'start':
                         return self.start(id)
                    
-                    elif text[0].lower() == 'tts':
-                        return self.tts(id)
+                    elif text[0].lower() == 'eko':
+                        return self.eko1(id)
                     elif text[0].lower() == 'gs':
                         return self.geo(id)
                     elif text[0].lower() == 'menu':
